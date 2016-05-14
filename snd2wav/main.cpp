0b297c48183ac342d08fe4c4400cd22cf90b8d9e
Uli Kusterer <ulivc@zathras.de>
2016-05-15 00:08:07 +0200

    Initial Commit

---
+++ snd2wav/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  snd2wav
+//
+//  Created by Uli Kusterer on 15/05/16.
+//  Copyright © 2016 Uli Kusterer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
